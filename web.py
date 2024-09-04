3a4219209d63e90b4f963f6073fc6498f6749e52
Sherif <eniselka@abv.bg>
2024-09-04 18:46:13 +0300

    add some changes to the flask application

--- web.py
+++ web.py
@@ -2,16 +2,29 @@
 
 from flask import Flask
 from flask import render_template
+import os
 
 app = Flask(__name__)
 
+env = os.getenv('ENV', 'Development')
+
+@app.route("/hello")
+def hello():
+    return f"Hello, World! \n This is {env} environment!"
+
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return render_template('index.html')
 
 @app.route("/picture")
 def pic():
-        return render_template('index.html')
+    return render_template('index2.html')
+
+
+@app.route("/user/<username>")
+def show_user_profile(username):
+    return f"Hello, {username}!"
+
 
 @app.route("/friends")
 def salvador():
